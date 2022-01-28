9680e55a4b4bb64b5357045da5f8cfefce192a1f
kylecourounis <kyledoescode@gmail.com>
2022-01-28 11:14:21 -0500

    Begin separating Minecraft versions into branches

--- src/main/java/com/opum2/abilityrings/ModInfo.java
+++
@@ -1,6 +0,0 @@
-package com.opum2.abilityrings;
-
-public class ModInfo {
-    public static final String MOD_ID = "abilityrings";
-    public static final String MOD_NAME = "AbilityRings";
-}
--- src/main/java/com/opum2/abilityrings/items/ModRingItem.java
+++
@@ -1,9 +0,0 @@
-package com.opum2.abilityrings.items;
-
-import com.opum2.abilityrings.init.ModCreativeTab;
-
-public class ModRingItem extends ModItem {
-    public ModRingItem() {
-        super(ModItem.DEFAULT_PROPERTIES.stacksTo(1).tab(ModCreativeTab.MOD_TAB));
-    }
-}
